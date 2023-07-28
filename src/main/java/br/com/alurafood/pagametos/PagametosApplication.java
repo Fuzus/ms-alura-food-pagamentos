faa4fad5e870e5933ac3cd584c71607c3b5490dc
Fuzus <matheusdamata2000@gmail.com>
2023-07-28 14:54:51 -0300

    first commit

---
+++ src/main/java/br/com/alurafood/pagametos/PagametosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.pagametos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagametosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagametosApplication.class, args);
+	}
+
+}
